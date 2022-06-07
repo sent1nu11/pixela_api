15612fd3c8eb65d076367027c4bd110ddb9e097f
sent1nu11 <sent1nu11@gmail.com>
2022-06-06 22:29:56 -0600

    update

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-USERNAME = "tufbot"
-TOKEN = "password"
+
+USERNAME = "tufbot2"
+TOKEN = "pqwerisf"
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 user_params = {
     "token": TOKEN,
@@ -11,7 +14,33 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
+#
+# result = requests.post(url=pixela_endpoint, json=user_params)
+#
+# print(result.text)
 
-result = requests.post(url=pixela_endpoint, json=user_params)
+graph_params = {
+    "id": "graph1",
+    "name": "Minutes Learning",
+    "unit": "minutes",
+    "type": "int",
+    "color": "sora"
+}
 
-print(result.text)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{graph_endpoint}/graph1"
+
+pixel_data = {
+    "date": "20200531",
+    "quantity": "300"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+
+print(response.text)
